d9f2df9ae536a800256c5347b45bafda6089f7a9
GoKanaz <gokanaz.official@gmail.com>
2026-02-08 23:42:29 +0700

    Fix duplicate annotations dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -118,3 +118,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
+
+configurations.all {
+    exclude(group = "org.jetbrains", module = "annotations-java5")
+}
