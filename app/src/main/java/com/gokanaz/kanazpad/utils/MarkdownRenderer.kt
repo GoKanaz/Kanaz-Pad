05d7614680a6902a258807d0b89e669943470516
GoKanaz <gokanaz.official@gmail.com>
2026-02-09 12:33:02 +0700

    Simplify MarkdownRenderer using CommonMark library

--- app/src/main/java/com/gokanaz/kanazpad/utils/MarkdownRenderer.kt
+++ app/src/main/java/com/gokanaz/kanazpad/utils/MarkdownRenderer.kt
@@ -2,48 +2,21 @@ package com.gokanaz.kanazpad.utils
 
 import android.content.Context
 import android.widget.TextView
-import io.noties.markwon.Markwon
-import io.noties.markwon.SoftBreakAddsNewLinePlugin
-import io.noties.markwon.ext.tables.TablePlugin
-import io.noties.markwon.html.HtmlPlugin
-import io.noties.markwon.image.ImagesPlugin
-import io.noties.markwon.syntax.Prism4jThemeDarkula
-import io.noties.markwon.syntax.SyntaxHighlightPlugin
-import io.noties.prism4j.Prism4j
-import io.noties.prism4j.annotations.PrismBundle
+import org.commonmark.parser.Parser
+import org.commonmark.renderer.html.HtmlRenderer
 
-@PrismBundle(includeAll = true)
 object MarkdownRenderer {
     
-    private var markwonInstance: Markwon? = null
-    
-    fun getMarkwon(context: Context): Markwon {
-        if (markwonInstance == null) {
-            val prism4j = Prism4j(GrammarLocatorDef())
-            
-            markwonInstance = Markwon.builder(context)
-                .usePlugin(ImagesPlugin.create())
-                .usePlugin(TablePlugin.create(context))
-                .usePlugin(HtmlPlugin.create())
-                .usePlugin(SoftBreakAddsNewLinePlugin.create())
-                .usePlugin(
-                    SyntaxHighlightPlugin.create(
-                        prism4j,
-                        Prism4jThemeDarkula.create()
-                    )
-                )
-                .build()
-        }
-        return markwonInstance!!
-    }
+    private val parser = Parser.builder().build()
+    private val renderer = HtmlRenderer.builder().build()
     
     fun renderMarkdown(context: Context, textView: TextView, markdown: String) {
-        val markwon = getMarkwon(context)
-        markwon.setMarkdown(textView, markdown)
+        val html = parseMarkdownToHtml(context, markdown)
+        textView.text = android.text.Html.fromHtml(html, android.text.Html.FROM_HTML_MODE_LEGACY)
     }
     
     fun parseMarkdownToHtml(context: Context, markdown: String): String {
-        val markwon = getMarkwon(context)
-        return markwon.toMarkdown(markdown).toString()
+        val document = parser.parse(markdown)
+        return renderer.render(document)
     }
 }
